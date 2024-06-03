8b93fd88df8492727812fefe9c666a5e9a00ecd9
KinuTheDragon <91227747+KinuTheDragon@users.noreply.github.com>
2024-06-03 12:56:26 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let coll = document.getElementsByClassName("collapsible");
+
+for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("collapsible-active");
+        let content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+        let oldText = this.innerHTML;
+        this.innerHTML = this.getAttribute("switch-text");
+        this.setAttribute("switch-text", oldText);
+    });
+}
